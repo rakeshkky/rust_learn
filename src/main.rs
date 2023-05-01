ad9bacc8757ff673fbdde94f9d8d84d0017ba2e6
Rakesh <12475069+rakeshkky@users.noreply.github.com>
2023-05-01 15:34:54 +0530

    If expressions

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,17 @@
 fn main() {
-    let mut x = 5;
+    let x = 31;
     println!("X is {}", x);
-    x = 6;
-    let y = 5.5;
-    print_numbers(x, y);
-    let z = 7;
-    let sum = add_numbers(x, z);
-    println!("Sum of X and Z is {}", sum);
+    let result = branch(x);
+    println!("Result is {}", result);
 }
 
-fn print_numbers(x: i32, y: f32) -> () {
-    // The following is a statement ending with a ;
-    // The functions is not returning anything
-    println!("X is {}, Y is {}", x, y);
-}
-
-fn add_numbers(x: i32, y: i32) -> i32 {
-    // Following is expression not ending with a ;
-    // Value should be returned through an expression
-    x + y
+fn branch(x : i32) -> i32 {
+    if x > 40 {
+        println!("X is greater than 40, mulitplying with 2");
+        x * 2
+    }
+    else {
+        println!("X is less than 40, mulitplying with 3");
+        x * 3
+    }
 }
