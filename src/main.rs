174b18cb667fa6f9a27028df9f373f6d7621a6ce
Rakesh <12475069+rakeshkky@users.noreply.github.com>
2023-05-01 15:45:40 +0530

    Tuples and Arrays

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,9 @@
 fn main() {
-    let x = 31;
-    println!("X is {}", x);
-    let result = branch(x);
-    println!("Result is {}", result);
-}
-
-fn branch(x : i32) -> i32 {
-    if x > 40 {
-        println!("X is greater than 40, mulitplying with 2");
-        x * 2
-    }
-    else {
-        println!("X is less than 40, mulitplying with 3");
-        x * 3
-    }
+    let my_tuple: (i32, f32, bool) = (5, 5.5, true);
+    // Array is initialized with size. Has fixed size and equivalent to primitive types
+    // Stored in a stack
+    // The following is equivalent to 'let my_array = [1, 2, 3, 4];'
+    let my_array: [i32; 4] = [1, 2, 3, 4];
+    println!("my_tuple 2nd element is {}", my_tuple.1);
+    println!("my_array 2nd element is {}", my_array[1]);
 }
