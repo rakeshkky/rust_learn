f9365f98b807bff7d9a6302865226779ce81392c
Rakesh <12475069+rakeshkky@users.noreply.github.com>
2023-05-01 18:36:40 +0530

    Passing variable to a function gives up ownership

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 fn main() {
-    let len = {
-        let s1 = String::from("123");
-        // Below line transfers ownership of Heap from s1 to s2
-        let mut s2 = s1; // s1 is invalidated here. So using of s1 after causes compile errors.
-        s2.push_str("456");
-        // s1.push_str("456"); causes compile error
-        s2.len()
-    };
-    println!("{}", len);
+    let mut s1 = String::from("123");
+    s1.push_str("456");
+    print_length(s1); // Ownership of s1 is transfered to the print_length function. Heap memory is freed.
+    // Using s1 again here causes compile error
+    // s1.push_str("456"); error
+}
+
+fn print_length(s: String) -> () {
+    let mut s_mut = s;
+    s_mut.push_str("789");
+    println!("Size of string, {}, is {}", s_mut, s_mut.len());
 }
