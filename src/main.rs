f107e4d8fedac1215b9a52f2a94d5c66814806d9
Rakesh <12475069+rakeshkky@users.noreply.github.com>
2023-05-01 18:46:15 +0530

    Passing mutable references to a function

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 fn main() {
     let mut s1 = String::from("123");
     s1.push_str("456");
-    print_length(&s1); // Ownership of s1 is borrowed by the print_length function through the reference.
-    s1.push_str("789"); // Works now
+    print_length(&mut s1); // Ownership of s1 is borrowed by the print_length function through the reference.
     println!("Final string is {}", s1);
 }
 
-fn print_length(s: &String) -> () {
+fn print_length(s: &mut String) -> () {
+    s.push_str("789");
     println!("Size of string, {}, is {}", s, s.len());
 }
