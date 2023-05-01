dfe86095e8ae197b9d1ba62956736fa940ce8593
Rakesh <12475069+rakeshkky@users.noreply.github.com>
2023-05-01 18:06:52 +0530

    scope of primitive types

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 fn main() {
-    let my_tuple: (i32, f32, bool) = (5, 5.5, true);
-    // Array is initialized with size. Has fixed size and equivalent to primitive types
-    // Stored in a stack
-    // The following is equivalent to 'let my_array = [1, 2, 3, 4];'
-    let my_array: [i32; 4] = [1, 2, 3, 4];
-    println!("my_tuple 2nd element is {}", my_tuple.1);
-    println!("my_array 2nd element is {}", my_array[1]);
+    let k = 1;
+    let j = {
+        // This k is different from outside k since the scopes are different
+        let k = 2;
+        println!("K is {} inside scope", k);
+        k +2
+    };
+    println!("J is {}", j);
+    println!("K is {} outside scope", k);
 }
