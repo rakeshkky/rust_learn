342438d37a76a971066b9bbac84601d44cd56ca3
Rakesh <12475069+rakeshkky@users.noreply.github.com>
2023-05-01 18:22:55 +0530

    String type shallow copying

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 fn main() {
-    let mut str = "Hello World!";
-    println!("{}", str);
-    str = "Hello World Again!";
-    println!("{}", str);
+    let len = {
+        let s1 = String::from("123");
+        // Below line transfers ownership of Heap from s1 to s2
+        let mut s2 = s1; // s1 is invalidated here. So using of s1 after causes compile errors.
+        s2.push_str("456");
+        // s1.push_str("456"); causes compile error
+        s2.len()
+    };
+    println!("{}", len);
 }
