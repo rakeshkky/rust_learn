3fdedc17d192aa7921cdc7467b3b6092b7682818
Rakesh <12475069+rakeshkky@users.noreply.github.com>
2023-05-01 18:41:52 +0530

    Passing references to a function keep the ownership

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 fn main() {
     let mut s1 = String::from("123");
     s1.push_str("456");
-    print_length(s1); // Ownership of s1 is transfered to the print_length function. Heap memory is freed.
-    // Using s1 again here causes compile error
-    // s1.push_str("456"); error
+    print_length(&s1); // Ownership of s1 is borrowed by the print_length function through the reference.
+    s1.push_str("789"); // Works now
+    println!("Final string is {}", s1);
 }
 
-fn print_length(s: String) -> () {
-    let mut s_mut = s;
-    s_mut.push_str("789");
-    println!("Size of string, {}, is {}", s_mut, s_mut.len());
+fn print_length(s: &String) -> () {
+    println!("Size of string, {}, is {}", s, s.len());
 }
