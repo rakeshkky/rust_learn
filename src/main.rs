039d6504fafc6aa3ee779e6e3f32edde250c458f
Rakesh <12475069+rakeshkky@users.noreply.github.com>
2023-05-01 19:23:07 +0530

    Enums - pass enum value by reference

--- src/main.rs
+++ src/main.rs
@@ -12,16 +12,18 @@ struct User {
 
 fn main() {
     // constructing a User value
-    let my_user = User {
+    let mut my_user = User {
         name: String::from("Rakesh"),
         age: 27,
         contact: Contact::Email(String::from("random@random.com")),
     };
-    let contact_string = contact_to_string(my_user.contact);
+    let contact_string = contact_to_string(&my_user.contact);
     println!("my_user name is {}, age is {} and a way of contact is {}", my_user.name, my_user.age, contact_string);
+    my_user.contact = Contact::PhoneNumber(String::from("9632587410"));
+    println!("Contact is {}", contact_to_string(&my_user.contact));
 }
 
-fn contact_to_string(c: Contact) -> String {
+fn contact_to_string(c: &Contact) -> String {
     match c {
         Contact::Email(email) => {
             format!("email, {}", email)
