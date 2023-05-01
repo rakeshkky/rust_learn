5a3747ac9f5fab76833ec02ba05ac4315aead59d
Rakesh <12475069+rakeshkky@users.noreply.github.com>
2023-05-01 19:41:32 +0530

    use Option<T> generic type

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ impl Contact {
 
 struct User {
     name: String,
-    age: i32,
+    age: Option<i32>,
     contact: Contact,
 }
 
@@ -20,15 +20,18 @@ fn main() {
     // constructing a User value
     let mut my_user = User {
         name: String::from("Rakesh"),
-        age: 27,
+        age: Option::None,
         contact: Contact::Email(String::from("random@random.com")),
     };
     // Using implementation to_string() on Contact
     let contact_string = my_user.contact.to_string();
-    println!("my_user name is {}, age is {} and a way of contact is {}", my_user.name, my_user.age, contact_string);
+    let age_string = age_to_string(&my_user.age);
+    println!("my_user name is {}, age is {} and a way of contact is {}", my_user.name, age_string, contact_string);
     my_user.contact = Contact::PhoneNumber(String::from("9632587410"));
     // Using function contact_to_string();
     println!("Contact is {}", contact_to_string(&my_user.contact));
+    my_user.age = Option::Some(27);
+    println!("Age is {}", age_to_string(&my_user.age));
 }
 
 fn contact_to_string(c: &Contact) -> String {
@@ -38,6 +41,17 @@ fn contact_to_string(c: &Contact) -> String {
         },
         Contact::PhoneNumber(phone_number) => {
             format!("phone number, {}", phone_number)
+        }
+    }
+}
+
+fn age_to_string(age: &Option<i32>) -> String {
+    match age {
+        Option::Some(age_num) => {
+            format!("{}", age_num)
+        },
+        Option::None => {
+            "None".to_string()
         }
     }
 }
