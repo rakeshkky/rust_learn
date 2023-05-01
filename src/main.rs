d9a4d544906f8f116a06d05156bf7f8ef76ce1f1
Rakesh <12475069+rakeshkky@users.noreply.github.com>
2023-05-01 18:12:18 +0530

    Mutable string literals

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,6 @@
 fn main() {
-    let k = 1;
-    let j = {
-        // This k is different from outside k since the scopes are different
-        let k = 2;
-        println!("K is {} inside scope", k);
-        k +2
-    };
-    println!("J is {}", j);
-    println!("K is {} outside scope", k);
+    let mut str = "Hello World!";
+    println!("{}", str);
+    str = "Hello World Again!";
+    println!("{}", str);
 }
