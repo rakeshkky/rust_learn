46928b7736322186f0f954c1a50c33efe63d7fb9
Rakesh <12475069+rakeshkky@users.noreply.github.com>
2023-05-01 18:56:21 +0530

    A mutable user variable

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,11 @@ struct User {
 
 fn main() {
     // constructing a User value
-    let my_user = User {
+    let mut my_user = User {
         name: String::from("Rakesh"),
         email: String::from("random@random.com"),
         age: 27
     };
+    my_user.name = "Rakesh Updated".to_string();
     println!("my_user name is {}, age is {} and email is {}", my_user.name, my_user.age, my_user.email);
 }
