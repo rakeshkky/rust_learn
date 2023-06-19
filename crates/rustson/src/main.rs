7a37d0ffd744935ee14469b5584655114c2ae63a
Rakesh <12475069+rakeshkky@users.noreply.github.com>
2023-06-19 17:27:14 +0530

    rustson: add json parsing code using serde

--- crates/rustson/src/main.rs
+++ crates/rustson/src/main.rs
@@ -1,3 +1,29 @@
+use serde::*;
+
+// User type
+#[derive(Serialize, Deserialize)]
+struct User {
+    name: String,
+    age: i32
+}
+
 fn main() {
-    println!("Hello, world!!");
+    let json_str = "[{\"name\":\"Rakesh\",\"age\":26},{\"name\":\"Ammu\",\"age\":22}]";
+    let users_result = serde_json::from_str::<Vec<User>>(&json_str);
+    match users_result {
+        Ok(users) => {
+            let json_result = serde_json::to_string(&users);
+            match json_result {
+                Ok(json_str) => {
+                    println!("{}", json_str);
+                },
+                Err(e) => {
+                    println!("Error occurred while encoding to json string - {}", e.to_string());
+                }
+            }
+        },
+        Err(e) => {
+            println!("Error occurred while parsing json string - {}", e.to_string());
+        }
+    }
 }
