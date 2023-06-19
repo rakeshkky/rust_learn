db1f421f80a2154cea4a5fcfc7a645904b87fd64
Rakesh <12475069+rakeshkky@users.noreply.github.com>
2023-06-19 21:04:43 +0530

    add clap for cli; parse and print json file with yaml support

--- crates/rustson/src/main.rs
+++ crates/rustson/src/main.rs
@@ -1,29 +1,43 @@
-use serde::*;
+use clap::Parser;
+use std::fs;
 
-// User type
-#[derive(Serialize, Deserialize)]
-struct User {
-    name: String,
-    age: i32
+// CLI Args
+#[derive(Parser, Debug)]
+#[command(version,author, about)]
+struct Args {
+    /// A JSON file
+    #[arg(short, long, value_name = "FILEPATH")]
+    file: String,
+
+    #[arg(long, default_value_t = false)]
+    to_yaml: bool
 }
 
 fn main() {
-    let json_str = "[{\"name\":\"Rakesh\",\"age\":26},{\"name\":\"Ammu\",\"age\":22}]";
-    let users_result = serde_json::from_str::<Vec<User>>(&json_str);
-    match users_result {
-        Ok(users) => {
-            let json_result = serde_json::to_string(&users);
-            match json_result {
-                Ok(json_str) => {
-                    println!("{}", json_str);
+    let args = Args::parse();
+    let file_path = args.file;
+    let file_str_result = fs::read_to_string(&file_path);
+    match file_str_result {
+        Ok(file_str) => {
+            let json_value_result = serde_json::from_str::<serde_json::Value>(&file_str);
+            match json_value_result {
+                Ok(json_value) => {
+                    if args.to_yaml {
+                        // convert to yaml
+                        let yaml_value = serde_yaml::from_str::<serde_yaml::Value>(&json_value.to_string()).unwrap();
+                        println!("{}", serde_yaml::to_string(&yaml_value).unwrap());
+                    } else {
+                        // pretty print the json
+                        println!("{}", serde_json::to_string_pretty(&json_value).unwrap());
+                    }
                 },
                 Err(e) => {
-                    println!("Error occurred while encoding to json string - {}", e.to_string());
+                    println!("Not a valid JSON file at {file_path}: {}", e.to_string());
                 }
             }
         },
         Err(e) => {
-            println!("Error occurred while parsing json string - {}", e.to_string());
+            println!("Unable to read file at {file_path}: {}", e.to_string());
         }
     }
 }
